0f60e53908cf684fd99a917e761957e92941e5b3
Harshada Kulkarni <126339178+Harshada1902@users.noreply.github.com>
2024-06-12 12:50:53 +0530

    Add files via upload

---
+++ datetime.cs
@@ -0,0 +1,13 @@
+using System;
+
+class CurrentDateTime
+{
+    static void Main(string[] args)
+    {
+        // Get the current date and time
+        DateTime now = DateTime.Now;
+
+        // Display the current date and time
+        Console.WriteLine("The current date and time is: {0}", now);
+    }
+}
